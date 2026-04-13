f5af8edea5eae9afd2b24cafd41745385883f564
Bignon7 <hindemelais@gmail.com>
2026-04-13 19:04:08 +0100

    Main page

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
 
 
 
-st.title("Cold Mail Generator")
 
-url_input = st.text_input("Enter a URL: ", value="https://careers.nike.com/fr/retail-associate-pt-nike-staten-island-14-29-hours-week/job/R-79939")
-submit_button = st.button("Submit")
+def create_streamlit_app(llm, portfolio, clean_text):
+    st.title("Cold Mail Generator")
 
-if submit_button:
-    st.code("Hello Hiring Manager, I am from AtliQ", language="markdown")
+    url_input = st.text_input("Enter a URL: ", value="https://careers.nike.com/fr/retail-associate-pt-nike-staten-island-14-29-hours-week/job/R-79939")
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        try:
+            loader = WebBaseLoader([url_input])
+            st.code("Hello Hiring Manager, I am from AtliQ", language="markdown")
+
+        except Exception as e:
+            print("Lala is kind")
